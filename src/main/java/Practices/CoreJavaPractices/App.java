31e9c5bef137e004d1a6b14f614d50b8ce377f65
ashimgit <ashimnb87@gmail.com>
2020-06-28 19:19:59 +0530

    initial commit

---
+++ src/main/java/Practices/CoreJavaPractices/App.java
@@ -0,0 +1,13 @@
+package Practices.CoreJavaPractices;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
